3865116b69d6201bf78fdbeb0f5b40ee9a0f1eff
Siddhesh <S.Rane@bayanpay.sa>
2026-04-21 12:45:46 +0530

    fonts added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'dashboard.dart';
 
 void main() {
   runApp(ProviderScope(child: const MyApp()));
 }
+
+final themeData = ThemeData(
+  colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+  textTheme: GoogleFonts.aBeeZeeTextTheme().copyWith(
+    bodySmall: GoogleFonts.aBeeZee(),
+    bodyMedium: GoogleFonts.aBeeZee(),
+    bodyLarge: GoogleFonts.aBeeZee(),
+  )
+);
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
@@ -15,9 +25,7 @@ class MyApp extends ConsumerWidget {
 
     return MaterialApp(
       title: 'RiverPod Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: themeData,
       home: const Dashboard(),
     );
   }
